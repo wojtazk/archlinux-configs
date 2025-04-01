a01c0d049640dbb6f3cce7265a2b7d673cde6347
Wojciech Kowal <48928433+wojtazk@users.noreply.github.com>
2025-04-01 22:59:43 +0200

    Update config.fish

--- config.fish
+++ config.fish
@@ -4,7 +4,7 @@ set fish_greeting ""
 # aliases
 alias ip='ip -color=always'
 alias ls='eza --color=always --group-directories-first --icons'
-alias update='yay && flatpak update'
+alias update='yay --noconfirm && flatpak update --assumeyes'
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
